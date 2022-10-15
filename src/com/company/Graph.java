6d43396fbd0790d563ca9c4a6cfe5651e03b503c
DucDuongNGUYEN <duc-duong.NGUYEN@etu.univ-amu.fr>
2022-10-15 11:26:55 +0200

    Initial commit

---
+++ src/com/company/Graph.java
@@ -0,0 +1,112 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Graph<Label> {
+
+    private class Edge {
+        public int source;
+        public int destination;
+        public Label label;
+
+        public Edge(int from, int to, Label label) {
+            this.source = from;
+            this.destination = to;
+            this.label = label;
+        }
+    }
+    private int cardinal;
+    private ArrayList<LinkedList<Edge>> incidency;
+    private ArrayList<Integer> visited = new ArrayList<Integer>();
+
+    public Graph(int size) {
+        cardinal = size;
+        incidency = new ArrayList<LinkedList<Edge>>(size+1);
+        for (int i = 0;i<cardinal;i++) {
+            incidency.add(i, new LinkedList<Edge>());
+        }
+    }
+
+    public int order() {
+        return cardinal;
+    }
+
+    public void addArc(int source, int dest, Label label) {
+        incidency.get(source).addLast(new Edge(source,dest,label));
+    }
+
+    public String toString() {
+        String result = "";
+        result = result + cardinal + "\n";
+        for (int i = 0; i < cardinal; i++) {
+            for (Edge e : incidency.get(i)) {
+                result = result + e.source + " " + e.destination + " "
+                        + e.label.toString() + "\n";
+            }
+        }
+        return result;
+    }
+
+    //Parcours en profondeur(DFS)
+    public void Explore(Edge e, LinkedList<Integer> result) {
+        int v = e.destination;
+        if (!visited.contains(v)) {
+            visited.add(v);
+            for (Edge edge : incidency.get(v)){
+                Explore(edge, result);
+            }
+            result.addFirst(v);
+        }
+    }
+    //Parcours en profondeur du graphe transposé
+    public Graph graphTranspose() {
+        Graph<Integer> graph = new Graph(cardinal);
+        int index = 0;
+        for (int i = 0; i < this.incidency.size(); i++) {
+            int dest = i;
+            LinkedList<Edge> edges = incidency.get(i);
+            for (Edge edge : edges) {
+                graph.addArc(edge.destination, dest, index++);
+            }
+        }
+        return graph;
+    }
+    //ÉTAPE 1 : l’algorithme de Kosaraju qui effectue DFS sur le graphique d’origine
+    public LinkedList<Integer> DFS_1() {
+        LinkedList<Integer> result = new LinkedList<>();
+        for (int i = 0; i < cardinal; i++) {
+            if (!visited.contains(i)) {
+                visited.add(i);
+                LinkedList<Edge> destinations = incidency.get(i);
+                for (Edge edge : destinations) {
+                    Explore(edge, result);
+                }
+                result.addFirst(i);
+            }
+        }
+        return result;
+    }
+    //ÉTAPE 2 : l’algorithme de Kosaraju qui effectue DFS sur le graphe inverse.
+    //Cela permettra d’identifier les chaînes fortement connectées
+    public ArrayList<LinkedList<Integer>> DFS_2(LinkedList<Integer> order) {
+        ArrayList<LinkedList<Integer>> result = new ArrayList<>();
+        for (Integer element : order) {
+            if (!visited.contains(element)) {
+                visited.add(element);
+                LinkedList<Integer> components = new LinkedList<>();
+                components.add(element);
+                for (Edge edge : incidency.get(element)) {
+                    if(!visited.contains(edge.destination)) {
+                        Explore(edge, components);
+                    }
+                }
+                result.add(components);
+            }
+        }
+        System.out.println("Composantes fortement connexes: " + result);
+        return result;
+    }
+
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    private final static File FILE_URL = new File("C:\\LICENCE 3\\Algorithmique 2\\formule-2-sat.txt");
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader(FILE_URL);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        br.readLine();
+        line = br.readLine();
+        String [] content = line.split(" ");
+        int size = Integer.parseInt(content[2]) * 2;
+        Graph<Integer> graph = new Graph<>(size);
+        int index = 0;
+        while ((line = br.readLine()) != null) {
+            String[] arg = line.split(" ");
+            int source1 = -Integer.parseInt(arg[0]);
+            int dest1 = Integer.parseInt(arg[1]);
+
+            int source2 = -Integer.parseInt(arg[1]);
+            int dest2 = Integer.parseInt(arg[0]);
+
+            graph.addArc(literalToVertex(source1), literalToVertex(dest1), index++);
+            graph.addArc(literalToVertex(source2), literalToVertex(dest2), index++);
+
+        }
+        Graph graphTrans = graph.graphTranspose();
+        System.out.println("Graph G-origine: ");
+        System.out.println(graph);
+        System.out.println("Graph G-trans: ");
+        System.out.println(graphTrans.toString());
+        System.out.println(is2Satisfiable(graphTrans.DFS_2(graph.DFS_1())));
+
+    }
+    public static int  literalToVertex(int l){
+        if (l >= 0)
+            return 2*(l-1);
+        return -2*l-1;
+    }
+    public static int vertexToLiteral(int s){
+        if (s%2==0)
+            return (s/2)+1;
+        return -(s-1)/2;
+    }
+    public  static boolean is2Satisfiable(ArrayList<LinkedList<Integer>> components){
+        for (int i = 0; i < components.size(); i++) {
+            for (Integer e : components.get(i)) {
+                //LA FORMULE EST INSATISFAISANTE s'il existe -x et x dans le même SCC
+                if(components.get(i).contains(-vertexToLiteral(e))){
+                    System.out.println("The given expression is unsatisfiable");
+                    return false;
+                }
+            }
+        }
+        //LA FORMULE EST SATISFAISANTE s'il n'existe pas -x et x dans le même SCC
+        System.out.println("The given expression is satisfiable");
+        return true;
+    }
+
+}
